73ad4ac34d17fa7b82d3263f329a615cbd4e71cf
AhmedFarrukh <af4232@nyu.edu>
2023-08-01 00:53:12 +0500

    basic structure

--- TrafficFlow.py
+++ TrafficFlow.py
@@ -4,13 +4,17 @@
 # d = minimum safety distance (m)
 # N = number of vehicles in the system
 # L = total length of the track (m)
-# t = step-size
+# h = step-size
 # T = horizon time
 # d = the minimum safety distance between two car bumpers
 # s = distance between the car and the one immediately in front
 
 import numpy as np
 import random
+import math
+
+def speed(s, v_f, tau, d):
+    return v_f*(1-math.exp(-1/(v_f*tau)*max(s-d,0)))
 
 #defining the ranges of the speed variables
 v_f_max = 80/3.6
@@ -23,22 +27,38 @@ d_max = 8
 #set the parameter values
 N = 5
 L = 1000
-T = 36 #increase later
-t = 0.1
+T = 3600 #increase later
+h = 0.1
+H = int(T/h)
 
 #Randomly selecting unique speed parameters for each driver
 v_f = (v_f_max-v_f_min)*np.random.rand(N,1) + v_f_min
 d = (d_max-d_min)*np.random.rand(N,1) + d_min
 tau = (tau_max-tau_min)*np.random.rand(N,1) + tau_min
 
-#'tau' should be in multiples of 't'
-tau = (np.rint(tau/t))*t
+#'tau' should be in multiples of 'h'
+tau = (np.rint(tau/h))*h
+tau_h = tau/h
 
 #Initializing the problem
-s = np.zeros([N,T], dtype=float)
-v = np.zeros([N,T], dtype=float)
+s = np.zeros([N,H], dtype=float)
+v = np.zeros([N,H], dtype=float)
 
 s[:,0] = L/N
 v[:,0] = v_f[:,0]
 
 #Simulate Speeding Dynamics
+for t in range (1,H):
+    for n in range (0,N):
+        if n==0:
+            s[n,t] = s[n,t-1] + h*(v[N,t-1]-v[n,t-1]) 
+        else:
+            s[n,t] = s[n,t-1] + h*(v[n-1,t-1]-v[n,t-1])
+        s[n,t] = max(s[n,t],d[n,0])
+
+        #Speed Updates
+        if t < tau_h[n,0]:
+            v[n,t] = v[n,t-1]
+        else:
+            v[n,t] = speed(s[n,t-tau_h[n,0]],v_f[n,0],tau[n,0],d[n,0])
+
